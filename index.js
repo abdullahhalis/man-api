5301171c7e40522416860f77813cf9d9891b1825
Abdullah Hali Saputra <abdullahhali060@gmail.com>
2025-10-22 21:33:50 +0700

    fix: documentation not showing on vercel

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const router = require('./routes/route');
 const swaggerUi = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
+
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api-docs', swaggerUi.serveFiles(swaggerDocument), swaggerUi.setup(swaggerDocument, {
+  customCss: '.swagger-ui .topbar { display: none }, '
+}));
+
 app.use(router);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log("Server listen on port " + port));
